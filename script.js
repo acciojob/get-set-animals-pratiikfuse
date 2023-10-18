2d8633e6b7a54bc3f05998334ef533c6bedc8180
Pratik Fuse <pratikfuse20@gmail.com>
2023-10-18 11:38:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ class Animal {
 		return this.species;
 	}
 	makeSound(){
-		console.log("aslfkn");
+		console.log(`The ${this.species} makes a sound`);
 	}
 }
 
 class Dog extends Animal {
 bark(){
-		console.log("woof");
+		console.log(`woof`);
 	}
 }
 
